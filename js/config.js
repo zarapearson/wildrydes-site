6e2c3b7758f85f2a7fc834f9663327195e52ecbf
Zara Pearson <3889751+zarapearson@users.noreply.github.com>
2026-02-02 16:47:59 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_zayn9VjJv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7700nros30njq86pl6k6lg1hq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_OKvwjQhch', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'bn5t02s06vvn87k1qvgn67856', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
