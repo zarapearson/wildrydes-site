ca3e97520c27f2fe8449f9a22b21f2a09596b3ac
Zara Pearson <3889751+zarapearson@users.noreply.github.com>
2026-02-05 12:20:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://vl8lbwa0qg.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
